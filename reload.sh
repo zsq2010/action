e8e7f12f8248e5f645d61c9204db1f7f1edabcb5
zsq2010 <zsq2010@gmail.com>
2024-04-07 20:31:55 +0800

    修改action run-on镜像

---
+++ reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 使用 pgrep 检查是否存在与 myapp 相关的进程
+if pgrep -f myapp > /dev/null; then
+    # 存在相关进程，使用 pkill 杀死这些进程
+    pkill -f myapp
+    echo "相关进程已被杀死"
+else
+    echo "未找到相关进程"
+fi
+
+# 启动 myapp
+nohup ./myapp &
